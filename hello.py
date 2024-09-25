4ebc77224e45628af7ec0d7a379d6320431a0391
Sarp Kose <sarp.kose@gmail.com>
2024-09-25 14:38:25 -0400

    clean code

--- hello.py
+++ hello.py
@@ -1,16 +1,3 @@
-# from flask import Flask
-# app = Flask(__name__)
-
-# @app.route('/') 
-# def index():
-#     return '<h1>Hello World!</h1>'
-
-# @app.route('/user/<name>') 
-# def user(name):
-#     return '<h1>Hello, {}!</h1>'.format(name)
-
-
-
 from flask import Flask, render_template
 from flask_bootstrap import Bootstrap 
 from flask_moment import Moment
@@ -18,10 +5,6 @@ from flask_moment import Moment
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 moment = Moment(app)
-
-# @app.route('/') 
-# def index():
-#     return render_template('index.html')
 
 @app.route('/user/<name>') 
 def user(name):
